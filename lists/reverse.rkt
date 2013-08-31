15507e528c57f6936b114c1f9b87e1bb6f2cf19c
Josh Davis <josh@joshldavis.com>
2013-08-31 15:38:46 -0500

    Add Racket code to reverse a list

---
+++ lists/reverse.rkt
@@ -0,0 +1,20 @@
+; Reverse a list.
+;
+; Example:
+;   Input: '(a b c d)
+;   Output: '(d c b a)
+;   Process: Use append to create the list in reverse.
+;   Example: (append (append (append (append '(d) '()) '(c)) '(b)) '(a))
+(define (list-reverse l)
+  (if (null? l)
+    '()
+    (append (list-reverse (cdr l))
+          (list (car l)))))
+
+(define l '(a b c d))
+
+(display "Input List: ")
+(displayln l)
+
+(display "Output List ")
+(displayln (list-reverse l))
