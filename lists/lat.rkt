a7e981d8f371f71e38e20c6a86fc0f72a1836c76
Josh Davis <josh@joshldavis.com>
2013-08-31 16:15:25 -0500

    Add lat? procedure

---
+++ lists/lat.rkt
@@ -0,0 +1,49 @@
+; A lat is a list of atoms.
+;
+; Source:
+;   The Little Schemer, pg. 15
+;
+; Run:
+;   $ racket -r lists/lat.rkt
+;
+; Examples:
+;   Input: '()
+;   Output: #t
+;
+;   Input: '(a b c d)
+;   Output: #t
+;
+;   Input: '(a (b) c d)
+;   Output: #f
+(define (lat? l)
+  (if (null? l)
+    #t
+    (if (list? (car l))
+      #f
+      (lat? (cdr l)))))
+
+(define l0 '())
+(define l1 '(a b c d))
+(define l2 '(a (b) c d))
+
+(display "Input List: ")
+(displayln l0)
+
+(display "Output List ")
+(displayln (lat? l0))
+
+(displayln "")
+
+(display "Input List: ")
+(displayln l1)
+
+(display "Output List ")
+(displayln (lat? l1))
+
+(displayln "")
+
+(display "Input List: ")
+(displayln l2)
+
+(display "Output List ")
+(displayln (lat? l2))
