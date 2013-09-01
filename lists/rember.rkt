a8b11a80fbbca0190c15eb150c629d3c34429684
Josh Davis <josh@joshldavis.com>
2013-08-31 20:35:14 -0500

    Add lists/rember.rkt

---
+++ lists/rember.rkt
@@ -0,0 +1,51 @@
+; Returns the list with the first occurrence of the given atom removed.
+;
+; Run:
+;   $ racket -r lists/rember.rkt
+;
+; Examples:
+;   Input: a, '()
+;   Output: '()
+;
+;   Input: a, '(b a c)
+;   Output: '(b c)
+;
+;   Input: a, '(b a c a d)
+;   Output: '(b c a d)
+;
+; Source:
+;   The Little Schemer, pg. 33
+(define (rember? a l)
+  (cond
+    ((null? l) l)
+    ((eq? a (car l)) (cdr l))
+    (else (cons
+            (car l)
+            (rember? a (cdr l))))))
+
+(define a 'a)
+(define l0 '())
+(define l1 '(b a c))
+(define l2 '(b a c a d))
+
+(display "Input List: ")
+(displayln l0)
+
+(display "Output List ")
+(displayln (rember? a l0))
+
+(displayln "")
+
+(display "Input List: ")
+(displayln l1)
+
+(display "Output List ")
+(displayln (rember? a l1))
+
+(displayln "")
+
+(display "Input List: ")
+(displayln l2)
+
+(display "Output List ")
+(displayln (rember? a l2))
